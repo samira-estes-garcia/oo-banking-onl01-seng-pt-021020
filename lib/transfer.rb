20281ea01ea8a83d854b29fa7ad072a45dd1e910
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-05 20:11:48 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,6 +18,8 @@ class Transfer
       @sender.deposit(@amount * -1)
       @receiver.deposit(@amount)
       @status = "complete"
+    elsif @status == "complete"
+      "Transaction was already executed."
     else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
