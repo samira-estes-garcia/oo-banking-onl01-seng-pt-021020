acf4308cf1503ba3095db755c402c7d809044ede
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-05 20:28:49 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,31 @@
 class BankAccount
+  
+  attr_reader :name 
+  attr_accessor :balance, :status 
+  
+  @@all = []
+
+  def initialize(name, balance=1000, status="open")
+    @name = name
+    @balance = balance
+    @status = status
+    @@all << self
+  end
+  
+  def deposit(money)
+    @balance = @balance + money
+  end
+  
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid? 
+    @status == "open" && @balance > 0 ? true : false
+  end
+  
+  def close_account 
+    @status = "closed"
+  end
 
 end
