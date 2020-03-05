c39be1eccb2c2f69bd3d4f504d83814aab873ed7
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-05 20:14:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,6 +16,7 @@ class Transfer
   def execute_transaction
     if @sender.balance < @amount
       status = "rejected"
+      
     else
       @sender.balance -= @amount
       @receiver.balance += @amount
